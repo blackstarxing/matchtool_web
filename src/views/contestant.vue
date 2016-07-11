34e83e22f1aeb94761ce6e23330c41c717cc18ee
张余烽 <zhangyf@miqtech.com>
2016-07-11 18:32:12 +0800

    update

--- src/views/contestant.vue
+++ src/views/contestant.vue
@@ -7,53 +7,55 @@
 				<div class="m-operate f-cb">
 					<div class="l-btn f-fl">
 						<a href="javascript:void(0);" class="u-btn u-btn-add" @click="addplayer"><img src="../../static/images/plus.png" alt="">添加选手</a>
-						<a href="javascript:void(0);" class="u-btn u-btn-upset"></a>
+						<a href="javascript:void(0);" class="u-btn u-btn-upset" @click="upsetseat"></a>
 					</div>					
 					<div class="r-btn f-fr">
-						<a href="">导入</a>
-						<a href="">导出</a>
+						<!-- <a href="">导入</a> -->
+						<a href="" @click="exportExcel">导出</a>
 					</div>
 				</div>
 				<div class="m-tb">
 					<table class="account_table">
 	                    <tbody>
 	                    <tr>
-	                        <th>序号</th>
+	                        <th width="110px;">序号</th>
 	                        <th>选手名称</th>
 	                        <th>手机号码</th>
 	                        <th>QQ</th>
 	                        <th>身份证号码</th>
-	                        <th>已签到</th>
+	                        <th v-if="needsign==1">已签到</th>
 	                        <th width="135px;">操作</th>
 	                    </tr>
-	                    <tr v-for='member in memberlist.list'>
-	                        <td>{{$index+1}}</td>
-	                        <td>{{member.name}}</td>
-	                        <td>{{member.telephone}}</td>
-	                        <td>{{member.qq}}</td>
-	                        <td>{{member.idcard}}</td>
-	                        <td>
+	                    <tr v-for='member in memberlist.list' class="memberInfo">
+	                        <td class="isfill" v-if="member.isfill==1">{{member.rownum}}</td>
+	                        <td v-else>{{member.rownum}}</td>
+	                        <td class="memberName">{{member.name}}</td>
+	                        <td class="memberTel">{{member.telephone}}</td>
+	                        <td class="memberQQ">{{member.qq}}</td>
+	                        <td class="memberId">{{member.idcard}}</td>
+	                        <td v-if="needsign==1">
 	                        <section class="signed">
 								<div class="signbox">
-									<input type="checkbox" checked="{{member.signed}}" @click="signStatus">
+									<input type="checkbox" checked="" @click="signStatus" v-if="member.signed==1">
+									<input type="checkbox" @click="signStatus" v-else>
 									<label></label>
 								</div>
 							</section>
 							</td>
 	                        <td>
 	                        	<div class="option">
-	                        		<a href="" class="u-btn-write"></a><a href="" class="u-btn-delete"></a>
+	                        		<a href="javascript:void(0);" class="u-btn-write" data-id="{{member.id}}" @click="editMember"></a><a href="" class="u-btn-delete" data-id="{{member.id}}" @click="deleteMember"></a>
 	                        	</div>
 	                        </td>
 	                    </tr>
 	                    </tbody>
 	                </table>
 	                <div class="m-page">
-	                	<a href="" id="prev"></a>
+	                	<a href="" id="prev" @click="prevpage"></a>
 	                	<div class="pagination"><span class="current">{{memberlist.pageNumber}}</span>/<span>{{memberlist.pages}}</span></div>
-	                	<a href="" id="next"></a>
-	                	<input type="text">
-	                	<a href="" class="u-btn">跳转</a>
+	                	<a href="" id="next" @click="nextpage"></a>
+	                	<input type="text" id="pageto" @keyup="checkpage">
+	                	<a href="" class="u-btn" @click="gopage">跳转</a>
 	                </div>
 				</div>
 			</div>		
@@ -63,25 +65,30 @@
 	<div class="m-mask">
 		<div class="m-pop">
 			<div class="wrap">
-				<h3>添加选手</h3>
+				<h3>添加参赛选手</h3>
 				<a href="javascript:void(0);" class="u-btn-close" @click="closePop"></a>
 				<div class="m-lst">				
 					<label for="">选手名称：</label>
-					<input type="text" class="eventname" placeholder="请输入参赛者名称">
+					<input type="text" class="name" name="name" placeholder="请输入参赛者名称">
+					<div class="attention"></div>
 				</div>
 				<div class="m-lst">				
 					<label for="">手机号码：</label>
-					<input type="text" class="eventname" placeholder="请输入参赛者手机号码">
+					<input type="text" class="tel" name="tel" placeholder="请输入参赛者手机号码">
+					<div class="attention"></div>
 				</div>
 				<div class="m-lst">				
 					<label for="">QQ：</label>
-					<input type="text" class="eventname" placeholder="请输入参赛者QQ号码">
+					<input type="text" class="qq" name="qq" placeholder="请输入参赛者QQ号码">
+					<div class="attention"></div>
 				</div>
 				<div class="m-lst">				
 					<label for="">身份证：</label>
-					<input type="text" class="eventname" placeholder="请输入参赛者身份证号码">
+					<input type="text" class="idcard" name="idcard" placeholder="请输入参赛者身份证号码">
+					<div class="attention"></div>
 				</div>
-				<a href="javascript:void(0);" class="u-btn">添加</a>
+				<div class="member-id" style="display:none"></div>
+				<a href="javascript:void(0);" class="u-btn add-member" @click="setMember">添加</a>
 			</div>			
 		</div>
 	</div>
@@ -91,16 +98,16 @@ import topNav from '../components/topNav.vue'
   	export default {
        	data () {
     		return {
-      			memberlist:""
+      			memberlist:"",
+      			roundId:"",
+      			needsign:""
     		}
   		},
-   		ready: function () {
-     		var roundId=window.sessionStorage.getItem("eventid");
+   		ready: function () {     		
      		var _this=this;
-   			var parmstr=JSON.stringify({roundId:roundId,pageNumber:1});
-   			var parm={};
-   			parm.jsonInfo=parmstr;
-	        _this.$http.post('http://192.168.30.248:8088/event/round/group/member/list',parm).then(function(response) {
+     		_this.roundId=window.sessionStorage.getItem("eventid");
+     		_this.needsign=window.sessionStorage.getItem("needsign");
+	        _this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
 	        	console.log(response);
 	            _this.memberlist=response.data.object.pager;
 	        },function(response) {
@@ -109,14 +116,279 @@ import topNav from '../components/topNav.vue'
   		},
   		methods: {
     		addplayer: function(e){
+    			$(".wrap .attention").hide();
+    			$('.name').val("");
+		    	$('.tel').val("");
+		    	$('.qq').val("");
+		    	$('.idcard').val("");
+    			$('.wrap h3').html("添加参赛选手");
+    			$(".add-member").removeClass('edit-member').text("添加");
 		        $('.m-mask').show();
 		    },
 		    closePop: function(e){
 		        $('.m-mask').hide();
 		    },
+		    // 打乱选手位置
+		    upsetseat:function(e){
+		    	var _this=this;
+		    	var parm={eventId:_this.roundId};
+		    	_this.$http.get('event/round/groupSeat/random',parm).then(function(response) {
+		        	console.log(response.data);
+		        	if(response.data.code==1){
+		        		layer.msg('选手顺序已打乱',{offset:"0px"});
+		        		_this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
+				        	console.log(response.data);
+				        	_this.memberlist=response.data.object.pager;
+				        },function(response) {
+				            console.log(response);
+				        });
+		        	}else{
+		        		layer.msg(response.data.msg,{offset:"0px"});
+		        	}
+		        },function(response) {
+		            console.log(response);
+		        });
+		    },
+		    // 修改签到状态
 		    signStatus:function(e){
-		    	alert(123);
-		    }
+		    	var _this=this;
+		    	var _target=$(e.currentTarget);
+		    	var memberId=_target.parents(".memberInfo").find(".u-btn-delete").attr("data-id");
+		    	var sign=0;
+
+		    	if(_target.attr('checked')=="checked"){
+		    		_target.attr('checked',false);
+		    		sign=0;
+		    	}else{
+		    		_target.attr('checked',true);
+		    		sign=1;
+		    	}
+		    	var parm={memberId:memberId,sign:sign};
+		    	_this.$http.get('event/round/group/member/sign',parm).then(function(response) {
+		        	console.log(response.data.msg);
+		        	if(response.data.code==1){
+		        		layer.msg('签到状态已更改',{offset:"0px"});
+		        		_this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
+				        	console.log(response.data);
+				        	_this.memberlist=response.data.object.pager;
+				        },function(response) {
+				            console.log(response);
+				        });
+		        	}else{
+		        		layer.msg(response.data.msg,{offset:"0px"});
+		        	}
+		        },function(response) {
+		            console.log(response);
+		        });
+		     	console.log(parm);
+		    	console.log(sign);
+		    },
+		    // 编辑成员信息
+		    editMember:function(e){
+		    	$(".wrap .attention").hide();
+		    	var _target=$(e.currentTarget);
+		    	$(".add-member").addClass('edit-member').text("修改");
+		    	$('.wrap h3').html("修改成员信息");
+		    	$('.name').val(_target.parents(".memberInfo").find(".memberName").text());
+		    	$('.tel').val(_target.parents(".memberInfo").find(".memberTel").text());
+		    	$('.qq').val(_target.parents(".memberInfo").find(".memberQQ").text());
+		    	$('.idcard').val(_target.parents(".memberInfo").find(".memberId").text());
+		    	$('.member-id').html(_target.attr("data-id"));
+		    	$('.m-mask').show();
+		    },
+		    // 删除成员
+		    deleteMember:function(e){
+		    	e.preventDefault();
+		    	var _this=this;
+		    	var _target=$(e.currentTarget);
+		    	var parm={memberId:_target.attr("data-id")};
+		    	layer.confirm('你确定要删除该成员吗？', {
+				  	btn: ['确定','取消'], //按钮
+				  	move:false
+				}, function(){
+					_this.$http.get('event/round/group/member/del',parm).then(function(response) {
+			        	console.log(response.data);
+			        	if(response.data.code==1){
+			        		layer.msg('已删除',{offset:"0px"});
+			        		_this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
+					        	console.log(response.data);
+					        	_this.memberlist=response.data.object.pager;
+					        },function(response) {
+					            console.log(response);
+					        });
+			        	}else{
+			        		layer.msg(response.data.msg,{offset:"0px"});
+			        	}
+			        },function(response) {
+			            console.log(response);
+			        });
+				}, function(){
+				  	
+				});
+		    },
+		    // 保存成员信息
+		    setMember:function(e){
+		    	function errorPlacement(mes,element){
+			    	var errorTips=element.parents(".m-lst").find('div.attention');
+			    	if(mes!=""){
+			    		errorTips.show().html(mes);
+			    	}else{
+			    		errorTips.hide();
+			    	}			    				    	
+			    }
+			    function formValidate(){
+			    	var valid=true;
+			    	$(".wrap input").each(function(){
+			    		var $this=$(this);
+						var value=$this.val(),name=$this.attr('name');	
+			    		var message="";
+			    		if(name=="name"){
+			    			if(value==""){
+			    				valid=false;
+					    		message="选手名称不能为空";
+					    	}
+					    	errorPlacement(message,$this);
+			    		}else if(name=="tel"){
+			    			if(value==""){
+			    				valid=false;
+					    		message="手机号码不能为空";
+			    			}else if(!/^1([0-9]){10}$/.test(value)){
+			    				valid=false;
+					    		message="请输入正确的手机格式";
+			    			}
+			    			errorPlacement(message,$this);
+			    		}else if(name=="qq"){
+			    			if(value==""){
+			    				valid=false;
+					    		message="QQ号码不能为空";
+			    			}else if(!/^[1-9]\d{4,10}$/.test(value)){
+			    				valid=false;
+					    		message="请输入正确的QQ号码";
+			    			}
+			    			errorPlacement(message,$this);
+			    		}else if(name=="idcard"){
+			    			if(value==""){
+			    				valid=false;
+					    		message="身份证号码不能为空";
+			    			}else if(!/^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/.test(value)){
+			    				valid=false;
+					    		message="请输入正确的身份证号码";
+			    			}
+			    			errorPlacement(message,$this);
+			    		}
+				    	   
+				    });
+				    return valid;
+			    };
+		    	var _this=this;
+		    	var _target=$(e.currentTarget); 
+		    	if(formValidate()){
+		    		if(_target.hasClass('edit-member')){
+			    		var parmstr=JSON.stringify({roundId:_this.roundId,id:$('.member-id').html(),name:$('.name').val(),telephone:$('.tel').val(),qq:$('.qq').val(),idcard:$('.idcard').val()});
+				    	var parm={};
+				    	parm.memberJson=parmstr;
+				    	_this.$http.get('event/round/group/member/edit',parm).then(function(response) {
+				        	console.log(response.data);
+				        	if(response.data.code==0){
+				        		$('.m-mask').hide();
+				        		layer.msg('修改成功',{offset:"0px"});
+						    	_this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
+						        	console.log(response.data);
+						        	_this.memberlist=response.data.object.pager;
+						        },function(response) {
+						            console.log(response);
+						        });
+				        	}else{
+				        		$('.m-mask').hide();
+				        		layer.msg(response.data.msg,{offset:"0px"});
+				        	}
+				        	console.log(parm);
+				        },function(response) {
+				            console.log(response.data.msg);
+				        });
+			    	}else{
+			    		var parmstr=JSON.stringify({roundId:_this.roundId,name:$('.name').val(),telephone:$('.tel').val(),qq:$('.qq').val(),idcard:$('.idcard').val()});
+				    	var parm={};
+				    	parm.memberJson=parmstr;
+			    		_this.$http.get('event/round/group/member/add',parm).then(function(response) {
+				        	console.log(response.data);
+				        	if(response.data.code==1){
+				        		$('.m-mask').hide();
+				        		layer.msg('添加成功',{offset:"0px"});
+				        		_this.$http.post('event/round/group/member/list',{roundId:_this.roundId}).then(function(response) {
+						        	console.log(response.data);
+						        	_this.memberlist=response.data.object.pager;
+						        },function(response) {
+						            console.log(response);
+						        });
+				        	}else{
+				        		$('.m-mask').hide();
+				        		layer.msg(response.data.msg,{offset:"0px"});
+				        	}
+				        	console.log(parm);
+				        },function(response) {
+				            console.log(response.data.code);
+				        });
+			    	}
+		    	}
+		    },
+		    // 导出列表
+		    exportExcel:function(e){
+		    	e.preventDefault();
+		    	var _this=this;
+		    	var _target=$(e.currentTarget); 
+		    	window.location.href="http://192.168.30.248:8088/event/round/group/member/export?roundId="+_this.roundId;
+		    },
+		    // 翻页
+  			prevpage:function(e){
+  				e.preventDefault();
+  				var currentpage = this.memberlist.pageNumber;
+	    		if(currentpage>1){
+	    			currentpage--;
+	    			this.$http.post("event/round/group/member/list",{roundId:this.roundId,pageNumber:currentpage}).then(function(response){
+	    				this.memberlist=response.data.object.pager;
+		    		}, function(response){
+		    			console.log(response);
+		    		})
+	    		}
+	    		else{
+	    			layer.msg('没有上一页了',{offset:"0px"});
+	    		}
+  			},
+  			nextpage:function(e){
+  				e.preventDefault();
+  				var currentpage = this.memberlist.pageNumber,
+  					maxpage = this.memberlist.pages;
+	    		if(currentpage<maxpage){
+	    			currentpage++;
+	    			this.$http.post("event/round/group/member/list",{roundId:this.roundId,pageNumber:currentpage}).then(function(response){
+	    				this.memberlist=response.data.object.pager;
+		    		}, function(response){
+		    			console.log(response);
+		    		})
+	    		}
+	    		else{
+	    			layer.msg('没有下一页了',{offset:"0px"});
+	    		}
+  			},
+  			gopage:function(e){
+  				e.preventDefault();
+  				var pageNum=$('#pageto').val();
+  				this.$http.post("event/round/group/member/list",{roundId:this.roundId,pageNumber:pageNum}).then(function(response){
+	    				this.memberlist=response.data.object.pager;
+		    		}, function(response){
+		    			console.log(response);
+		    		})
+  			},
+  			checkpage:function(e){
+  				var pages = this.memberlist.pages; 
+		    	var num = $('#pageto').val();
+		    	if(num==0 && num!=""){
+		    		$('#pageto').val('1');
+		    	}else if(num>pages){
+		    		$('#pageto').val(pages);
+		    	}
+  			}
 	  	},
        components: {
           topNav
--- src/views/homepage.vue
+++ src/views/homepage.vue
@@ -4,7 +4,7 @@
 		<div class="g-bd" style="margin-top:20px;">
 			<div class="g-tp"></div>
 			<div class="g-list">
-				<h3>浙江省网娱大师-雪碧真英雄城市争霸赛</h3>
+				<h3>赛事列表</h3>
 				<div class="m-n-list m-search f-cb">			
 					<select class="mr20" v-model="eventRoundStatus" @change="eventStatus">
 						<option value="" selected="">我的全部赛事</option>
@@ -37,33 +37,35 @@
 	                        <th width="135px;">操作</th>
 	                        <th></th>
 	                    </tr>
-	                    <tr v-for='matchlist in matchlists.list' @click="linkTechpic">
+	                    <tr v-for='matchlist in matchlists.list'>
 	                        <td>{{$index+1}}</td>
 	                        <td>
 	                        	<div class="s-n-img">
-									<img src="../../static/images/bg.png">
+									<img v-bind:src="'http://img.wangyuhudong.com'+matchlist.poster">
 								</div>
 	                        </td>
-	                        <td><a href="">{{matchlist.eventName}}</a></td>
+	                        <td @click="linkTechpic"><a href="">{{matchlist.eventName}}</a></td>
 	                        <td>网娱大师官方赛事组</td>
 	                        <td>单阶段  单败制</td>
 	                        <td>{{matchlist.num}}/{{matchlist.maxNum}}</td>
-	                        <td>{{matchlist.createDate}}</td>
+	                        <td>{{new Date(matchlist.createDate).Format("yyyy-MM-dd hh:mm:ss")}}</td>
 	                        <td>
 	                        	<div class="option">
 	                        		<a href="javascript:void(0);" class="u-btn-delete" title="删除" data-id="{{matchlist.eventId}}" @click="deleteEvent"></a>
 	                        	</div>
 	                        </td>
 	                        <td class="get_eventid" style="display:none">{{matchlist.eventId}}</td>
+	                        <td class="get_roundid" style="display:none">{{matchlist.id}}</td>
+	                        <td class="get_needsign" style="display:none">{{matchlist.needSign}}</td>
 	                    </tr>
 	                    </tbody>
 	                </table>
 	                <div class="m-page">
-	                	<a href="" id="prev"></a>
+	                	<a href="" id="prev" @click="prevpage"></a>
 	                	<div class="pagination"><span class="current">{{matchlists.pageNumber}}</span>/<span>{{matchlists.pages}}</span></div>
-	                	<a href="" id="next"></a>
-	                	<input type="text">
-	                	<a href="" class="u-btn">跳转</a>
+	                	<a href="" id="next" @click="nextpage"></a>
+	                	<input type="text" id="pageto" @keyup="checkpage">
+	                	<a href="" class="u-btn" @click="gopage">跳转</a>
 	                </div>
 				</div>
 			</div>		
@@ -94,6 +96,23 @@ import topHead from '../components/topHead.vue'
 	        },function(response) {
 	            console.log(response);
 	        });
+
+	        // 日期格式化
+	        Date.prototype.Format = function (fmt) { //author: meizz 
+			    var o = {
+			        "M+": this.getMonth() + 1, //月份 
+			        "d+": this.getDate(), //日 
+			        "h+": this.getHours(), //小时 
+			        "m+": this.getMinutes(), //分 
+			        "s+": this.getSeconds(), //秒 
+			        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+			        "S": this.getMilliseconds() //毫秒 
+			    };
+			    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+			    for (var k in o)
+			    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+			    return fmt;
+			}
   		},
   		methods:{
   			searchEvent:function(e){
@@ -121,23 +140,106 @@ import topHead from '../components/topHead.vue'
 		            console.log(response);
 		        });
   			},
+  			// 删除赛事
   			deleteEvent:function(e){
   				var _this=this;
 				var _target=$(e.currentTarget);
 				var parmstr=JSON.stringify({oetInfoId:_target.attr("data-id")});
-	   			var parm={};
-	   			parm.jsonInfo=parmstr;
-				_this.$http.get('/event/delete',parm).then(function(response) {
-		        	console.log(response);
-		        },function(response) {
-		            console.log(response);
-		        });
+				layer.confirm('你确定要删除该项赛事吗？', {
+				  	btn: ['确定','取消'], //按钮
+				  	move:false
+				}, function(){
+				  	var parm={};
+		   			parm.jsonInfo=parmstr;
+					_this.$http.get('/event/delete',parm).then(function(response) {
+			        	console.log(response.data);
+			        	if(response.data.code==1){
+			        		layer.msg("删除成功",{offset:"0px"});
+			        		var reloadstr=JSON.stringify({eventName:this.eventName,eventRoundStatus:this.eventRoundStatus,pageNumber:this.matchlists.pageNumber});
+				   			var reload={};
+				   			reload.jsonInfo=reloadstr;
+			    			this.$http.get("/event/getEventRoundList",reload).then(function(response){
+			    				this.matchlists=response.data.object.pager;
+				    		}, function(response){
+				    			console.log(response);
+				    		})
+			        	}			        	
+			        },function(response) {
+			            console.log(response);
+			        });
+				}, function(){
+				  	
+				});
   			},
   			linkTechpic:function(e){
+  				e.preventDefault();
   				var _target=$(e.currentTarget);
-  				var _eventid=_target.find(".get_eventid").text();
+  				var _eventid=_target.parent().find(".get_eventid").text();
+  				var _roundid=_target.parent().find(".get_roundid").text();
+  				var _needsign=_target.parent().find(".get_needsign").text();
   				window.sessionStorage.setItem("eventid",_eventid);
+  				window.sessionStorage.setItem("roundid",_roundid);
+  				window.sessionStorage.setItem("needsign",_needsign);
   				this.$route.router.go({path: '/techPic'})
+  			},
+  			// 翻页
+  			prevpage:function(e){
+  				e.preventDefault();
+  				var currentpage = this.matchlists.pageNumber;
+	    		if(currentpage>1){
+	    			currentpage--;
+	    			var parmstr=JSON.stringify({eventName:this.eventName,eventRoundStatus:this.eventRoundStatus,pageNumber:currentpage});
+		   			var parm={};
+		   			parm.jsonInfo=parmstr;
+	    			this.$http.get("/event/getEventRoundList",parm).then(function(response){
+	    				this.matchlists=response.data.object.pager;
+		    		}, function(response){
+		    			console.log(response);
+		    		})
+	    		}
+	    		else{
+	    			layer.msg('没有上一页了',{offset:"0px"});
+	    		}
+  			},
+  			nextpage:function(e){
+  				e.preventDefault();
+  				var currentpage = this.matchlists.pageNumber,
+  					maxpage = this.matchlists.pages;
+	    		if(currentpage<maxpage){
+	    			currentpage++;
+	    			var parmstr=JSON.stringify({eventName:this.eventName,eventRoundStatus:this.eventRoundStatus,pageNumber:currentpage});
+		   			var parm={};
+		   			parm.jsonInfo=parmstr;
+	    			this.$http.get("/event/getEventRoundList",parm).then(function(response){
+	    				this.matchlists=response.data.object.pager;
+		    		}, function(response){
+		    			console.log(response);
+		    		})
+	    		}
+	    		else{
+	    			layer.msg('没有下一页了',{offset:"0px"});
+	    		}
+  			},
+  			gopage:function(e){
+  				e.preventDefault();
+  				var pageNum=$('#pageto').val();
+  				var parmstr=JSON.stringify({eventName:this.eventName,eventRoundStatus:this.eventRoundStatus,pageNumber:pageNum});
+	   			var parm={};
+	   			parm.jsonInfo=parmstr;
+    			this.$http.get("/event/getEventRoundList",parm).then(function(response){
+    				this.matchlists=response.data.object.pager;
+	    		}, function(response){
+	    			console.log(response);
+	    		})
+  			},
+  			checkpage:function(e){
+  				var pages = this.matchlists.pages; 
+		    	var num = $('#pageto').val();
+		    	if(num==0 && num!=""){
+		    		$('#pageto').val('1');
+		    	}else if(num>pages){
+		    		$('#pageto').val(pages);
+		    	}
   			}
   		},
 	  	components: {
