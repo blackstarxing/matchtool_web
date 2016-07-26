fdd1bb8499ad3cc94b43f39f7733ea7667437379
张余烽 <zhangyf@miqtech.com>
2016-07-26 10:14:51 +0800

    update

--- src/components/pageFoot.vue
+++ src/components/pageFoot.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="tech_footer">
-		<p>Copyright © 2016 浙江网竞网络科技有限公司未经许可不得复制、转载或摘编，违者必究!</p>
+		<p>Copyright © 2016 浙江网竞网络科技有限公司&nbsp&nbsp&nbsp未经许可不得复制、转载或摘编，违者必究!</p>
 		<p style="margin:10px 0 40px">浙ICP备13005497</p>
 		<img src="../../static/images/footer.png">
 	</div>
