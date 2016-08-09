a6593ef7a029aca264ed2618dd8a0bc06c965eaf
张余烽 <306206452@qq.com>
2016-08-09 18:44:01 +0800

    赛制设置 （待调试接口）

--- src/views/quickmatchformat.vue
+++ src/views/quickmatchformat.vue
@@ -16,7 +16,7 @@
 					<p class="g-c-l">赛事类型</p>
 					<div class="g-c-zbf">
 					    <div class="f-fl g-c-ms">
-							<input type="radio" id="online" name="matchitem" class="regular-radio" checked="checked" value="" />
+							<input type="radio" id="online" name="matchitem" class="regular-radio" checked="checked" value="1" v-model="formdata.type"/>
 							<label for="online"></label>
 							<label for="online" class="u-c-per">		
 								<span class="f-fl">
@@ -25,7 +25,7 @@
 							</label>
 						</div>
 						<div class="f-fl g-c-ms">
-							<input type="radio" id="offline" name="matchitem" class="regular-radio" value="" disabled />
+							<input type="radio" id="offline" name="matchitem" class="regular-radio" value="2" v-model="formdata.type" disabled />
 							<label for="offline"></label>
 							<label for="offline" class="u-c-per">		
 								<span class="f-fl col8f">
@@ -34,52 +34,54 @@
 							</label>
 						</div>
 					</div>
-					<p class="g-c-l mt40">采用赛制<span class="f-fr"><span class="g-help"></span>查看赛制帮助</span></p>
-					<div class="clearfix mb12">
-						<div class="f-fl g-c-sz" @click="cherksz">
-							<p class="g-c-szt">单败淘汰制</p>
-							<div class="g-c-szm">
-								<img src="../../static/images/danbai.png" class="mt16">
+					<div class="f-c">
+						<p class="g-c-l mt40">采用赛制<span class="colfdb f-tip"></span><span class="f-fr"><span class="g-help"></span>查看赛制帮助</span></p>
+						<div class="clearfix mb12">
+							<div class="f-fl g-c-sz" @click="cherksz" selected="1">
+								<p class="g-c-szt">单败淘汰制</p>
+								<div class="g-c-szm">
+									<img src="../../static/images/danbai.png" class="mt16">
+								</div>
+								<div class="g-c-sz-cherk">
+									<i class="g-sz-gimg"></i>
+								</div>
 							</div>
-							<div class="g-c-sz-cherk">
-								<i class="g-sz-gimg"></i>
-							</div>
-						</div>
-						<div class="f-fr g-c-sz">
-							<p class="g-c-szt">双败淘汰制</p>
-							<div class="g-c-szm">
-								<img src="../../static/images/shuanbai.png" class="mt5">
-							</div>
-						</div>
-					</div>
-					<div class="g-c-szgz">
-						<div class="g-tri-up">
-							<div class="g-tri-iup"></div>
-						</div>
-						<div class="f-fl mr56 g-c-kz-sz">
-							<input type="checkbox" id="dbsz" class="regular-checkboxs" name="" />
-							<label for="dbsz"></label>
-							<label for="dbsz" class="u-c-kz-sz">需要增加一场第三名争夺战</label>
-						</div>
-					</div>
-					<div class="clearfix">
-						<div class="f-fl g-c-sz">
-							<p class="g-c-szt">小组循环制</p>
-							<div class="g-c-szm">
-								<img src="../../static/images/danbai.png" class="mt16">
+							<div class="f-fr g-c-sz" selected="2">
+								<p class="g-c-szt">双败淘汰制</p>
+								<div class="g-c-szm">
+									<img src="../../static/images/shuanbai.png" class="mt5">
+								</div>
 							</div>
 						</div>
-						<div class="f-fr g-c-sz">
-							<p class="g-c-szt">小组积分制</p>
-							<div class="g-c-szm">
-								<img src="../../static/images/shuanbai.png" class="mt5">
+						<div class="g-c-szgz">
+							<div class="g-tri-up">
+								<div class="g-tri-iup"></div>
+							</div>
+							<div class="f-fl mr56 g-c-kz-sz">
+								<input type="checkbox" id="dbsz" class="regular-checkboxs" name="" />
+								<label for="dbsz"></label>
+								<label for="dbsz" class="u-c-kz-sz">需要增加一场第三名争夺战</label>
+							</div>
+						</div>
+						<div class="clearfix">
+							<div class="f-fl g-c-sz" selected="3">
+								<p class="g-c-szt">小组循环制</p>
+								<div class="g-c-szm">
+									<img src="../../static/images/danbai.png" class="mt16">
+								</div>
+							</div>
+							<div class="f-fr g-c-sz" selected="4">
+								<p class="g-c-szt">小组积分制</p>
+								<div class="g-c-szm">
+									<img src="../../static/images/shuanbai.png" class="mt5">
+								</div>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
 			<div class="confirmBtn mb120">
-				<a href="" class="nextBtn"><i></i>确认，进入赛制设置</a>
+				<a href="" class="nextBtn" @click="creatematch"><i></i>确认，并生成赛事</a>
 			</div>
 		</div>
 		<div class="steps">
@@ -102,7 +104,12 @@ import createPop from '../components/createPop.vue'
 	export default {
 		data () {
 			return{
-
+				formdata:{
+					eventRoundId:'',
+					needThird:'',
+					regime:'',
+					type:''
+				}
 			}
 		},
 		components:{
@@ -119,6 +126,50 @@ import createPop from '../components/createPop.vue'
 				var _this = $(event.target);
 				_this.parents('.g-c-sz').find('.g-c-sz-cherk').toggle();
 				$('.g-c-szgz').fadeToggle();
+			},
+			creatematch:function(event){
+				var _this = this;
+				event.preventDefault();
+				var slt = $('.g-c-sz-cherk').css('display') != 'none';
+				if(slt){
+					_this.formdata.regime = 1;
+				}
+				else{
+					_this.formdata.regime = '';
+				}
+				function errorPlacement(mes,element){
+		    		var errorTips=element.parents(".f-c").find('.f-tip');
+			    	if(mes!=""){
+			    		errorTips.css("display","inline-block").html(mes);	
+			    	}else{
+			    		errorTips.css("display","none");	
+			    	}
+		    	}
+		    	function formValidate(){
+		    		var valid=true;
+		    		var $this=$('#dbsz');
+		    		var message="请选择赛制！";
+		    		if(_this.formdata.regime ==''){
+		    			valid=false;
+		    			errorPlacement(message,$this);
+		    		}
+		    		else{
+		    			valid=true;
+		    			message='';
+		    			errorPlacement(message,$this);
+		    		}
+		    		return valid;
+		    	}
+
+				if($('#dbsz').is(':checked')){
+					_this.formdata.needThird = 0;
+				}
+				else{
+					_this.formdata.needThird = 1;
+				}
+				if(formValidate()){
+					alert('好了')
+				}
 			}
 		}
 	}
