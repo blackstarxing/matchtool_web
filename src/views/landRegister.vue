c6b203b047bbdf5f3572987184e85ea8a9cf44d2
陈兢花 <240985492@qq.com>
2016-08-12 17:00:09 +0800

    update nickname

--- src/views/landRegister.vue
+++ src/views/landRegister.vue
@@ -239,9 +239,11 @@ import createPop from '../components/createPop.vue'
 					if(response.data.code){
 						document.cookie="oetevent.login.sessionid="+response.data.object["oetevent.login.sessionid"];
 						document.cookie="oetevent.login.token="+response.data.object["oetevent.login.token"];
-			  			document.cookie="oetUser="+response.data.object.oetUser.id;
-			  			document.cookie="appUser="+response.data.object.appUser.id;
+			  			document.cookie="oetUserId="+response.data.object.oetUser.id;
+			  			document.cookie="appUserId="+response.data.object.appUser.id;
 			  			window.sessionStorage.setItem("appusericon",response.data.object.appUser.icon);
+			  			window.sessionStorage.setItem("nickname",response.data.object.appUser.nickname);
+			  			console.log(document.cookie.split(";"));
 						this.$route.router.go({path: '/quickmatch'}); 
 					}
 			      },function(response) {
