559cafaf73627bb93ac7e7c0a888099cc4956dc7
闫佳兴 <414061962@qq.com>
2016-08-15 16:08:41 +0800

    landingpage

---
+++ src/views/index.vue
@@ -0,0 +1,215 @@
+<template>
+	<top-head></top-head>
+	<side-bar></side-bar>
+	<slide-bar></slide-bar>
+	<create-pop></create-pop>
+	<div class="g-w" style="margin-top:90px;">
+		<div class="banner">
+			<img src="../../static/images/landing.jpg" alt="">
+		</div>
+		<div class="landing-box">
+			<h3>最新赛事</h3>
+			<div class="landing-content f-cb">
+				<div class="match">
+					<div class="pic">
+						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
+					</div>
+					<div class="match-progress">
+						<div class="match-name">
+							【英雄联盟】八月福利SOLO赛 死歌VS维鲁斯
+						</div>
+						<div class="list mid">
+							<div class="progress">
+						      <span class="orange" id="bar" style="width: 73.4375%;"><span></span></span>
+						    </div>
+						    <div>by 网娱大师官方赛事组<span class="f-col f-fr"><strong>47</strong>/64</span></div>
+						</div>
+					</div>			
+				</div>
+				<div class="match" style="margin:0 2%;">
+					<div class="pic">
+						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
+					</div>
+					<div class="match-progress">
+						<div class="match-name">
+							【英雄联盟】八月福利SOLO赛 死歌VS维鲁斯
+						</div>
+						<div class="list mid">
+							<div class="progress">
+						      <span class="orange" id="bar" style="width: 73.4375%;"><span></span></span>
+						    </div>
+						    <div>by 网娱大师官方赛事组<span class="f-col f-fr"><strong>47</strong>/64</span></div>
+						</div>
+					</div>			
+				</div>
+				<div class="match">
+					<div class="pic">
+						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
+					</div>
+					<div class="match-progress">
+						<div class="match-name">
+							【英雄联盟】八月福利SOLO赛 死歌VS
+						</div>
+						<div class="list mid">
+							<div class="progress">
+						      <span class="orange" id="bar" style="width: 73.4375%;"><span></span></span>
+						    </div>
+						    <div>by 网娱大师官方赛事组<span class="f-col f-fr"><strong>47</strong>/64</span></div>
+						</div>
+					</div>			
+				</div>
+			</div>
+		</div>
+		<div class="landing-box">
+			<div class="landing-content f-cb">
+				<div class="m-advertise">
+					<img src="../../static/images/ad.jpg">
+					<div class="ad-title">
+						《英雄联盟》秋季超级联赛资格赛报名开启
+					</div>
+				</div>
+				<div class="flip-container">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+				<div class="flip-container">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+				<div class="flip-container" style="margin-right:0;">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+				<div class="flip-container">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+				<div class="flip-container">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+				<div class="flip-container" style="margin-right:0;">
+					<div class="flipper">
+						<div class="front">
+							<!-- 前面内容 -->
+							<img src="../../static/images/flip.jpg">
+							<div class="flip-wrap">
+								<span>秋季超级联赛杭州站</span>
+							</div>
+						</div>
+						<div class="back">
+							<!-- 背面内容 -->
+							<h4>网娱大师赛事组</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
+							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+			<a href="" class="u-btn u-landing-btn">查看全部赛事</a>
+		</div>
+	</div>
+</template>
+<script type="text/javascript">
+import topHead from '../components/topHead.vue'
+import sideBar from '../components/sideBar.vue'
+import slideBar from '../components/slideBar.vue'
+import createPop from '../components/createPop.vue'
+	export default {
+		data () {
+			return{
+				
+			}
+		},
+		components:{
+			topHead,
+	        sideBar,
+	        slideBar,
+	        createPop
+		},
+		ready:function(){
+			
+
+		},
+		methods:{
+			
+		},
+		events:{
+			
+		}
+	}
+</script>
--- src/views/landingpage.vue
+++ src/views/landingpage.vue
@@ -13,7 +13,7 @@
 				<div class="match">
 					<div class="pic">
 						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
-						<div class="title"><i class="iconfont"></i><span style="margin-left:6px;margin-right:4px;">by</span><span class="f-col">网娱官方赛事组</span></div>
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
 					</div>
 					<div class="match-progress">
 						<div class="match-name">
@@ -30,7 +30,7 @@
 				<div class="match" style="margin:0 2%;">
 					<div class="pic">
 						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
-						<div class="title"><i class="iconfont"></i><span style="margin-left:6px;margin-right:4px;">by</span><span class="f-col">网娱官方赛事组</span></div>
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
 					</div>
 					<div class="match-progress">
 						<div class="match-name">
@@ -47,11 +47,11 @@
 				<div class="match">
 					<div class="pic">
 						<img src="http://img.wangyuhudong.com///uploads/2016/08/01/7ce9d883e8fb400eb95dfe9115055c54.jpg" alt="" width="100%">
-						<div class="title"><i class="iconfont"></i><span style="margin-left:6px;margin-right:4px;">by</span><span class="f-col">网娱官方赛事组</span></div>
+						<div class="title"><i class="iconfont"></i><span class="f-col">2016.08.06 18:00</span></div>
 					</div>
 					<div class="match-progress">
 						<div class="match-name">
-							【英雄联盟】八月福利SOLO赛 死歌VS维鲁斯
+							【英雄联盟】八月福利SOLO赛 死歌VS
 						</div>
 						<div class="list mid">
 							<div class="progress">
@@ -68,25 +68,58 @@
 				<div class="m-organize">
 					<img src="../../static/images/organize1.png">
 					<h3>多元化赛制模板</h3>
-					<p>高效便捷 一键生成赛事</p>
+					<p>高效便捷</p>
+					<p>一键生成赛事</p>
 				</div>
 				<div class="m-organize">
 					<img src="../../static/images/organize2.png">
 					<h3>赛事进程管理</h3>
-					<p>透明比赛进程 记录每个场次每局比分</p>
+					<p>透明比赛进程</p>
+					<p>记录每个场次每局比分</p>
 				</div>
 				<div class="m-organize">
 					<img src="../../static/images/organize3.png">
 					<h3>报名自动化管理</h3>
-					<p>选手自主报名 告别QQ群收集记录的繁琐</p>
+					<p>选手自主报名</p>
+					<p>告别QQ群收集记录的繁琐</p>
 				</div>
 				<div class="m-organize">
 					<img src="../../static/images/organize4.png">
 					<h3>个性化赛事主页</h3>
-					<p>打造品牌 沉淀每一场赛事</p>
+					<p>打造品牌</p>
+					<p>沉淀每一场赛事</p>
 				</div>
 			</div>
-			
+			<a href="" class="u-btn u-landing-btn">申请成为赛事组织者</a>
+		</div>
+		<div class="landing-box">
+			<div class="landing-content f-cb">
+				<div class="m-match">
+					<img src="../../static/images/match1.png">
+					<h3>海量赛事信息</h3>
+					<p>一手新鲜</p>
+					<p>不错过每一场竞赛</p>
+				</div>
+				<div class="m-match">
+					<img src="../../static/images/match2.png">
+					<h3>个人\战队一键报名</h3>
+					<p>报名管理</p>
+					<p>一次报名永久存储</p>
+				</div>
+				<div class="m-match">
+					<img src="../../static/images/match3.png">
+					<h3>对阵对手早知道</h3>
+					<p>随时查询</p>
+					<p>了解你的赛事进程</p>
+				</div>
+				<div class="m-match">
+					<img src="../../static/images/match4.png">
+					<h3>个人战绩中心</h3>
+					<p>沉淀战果</p>
+					<p>打造属于你的电竞档案</p>
+				</div>
+			</div>
+			<a href="" class="u-btn u-landing-btn u-enter-btn">立即加入</a>
 		</div>
 	</div>
 </template>
