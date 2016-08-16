8131d1a1a0904940a6681f9d92c73b0fc362ba3d
闫佳兴 <414061962@qq.com>
2016-08-16 14:59:34 +0800

    update

--- src/views/index.vue
+++ src/views/index.vue
@@ -10,7 +10,7 @@
 		<div class="landing-box" style="margin-bottom:0;">
 			<h3>赛事推荐</h3>
 			<div class="landing-content f-cb">
-				<div class="match" v-for="match in matchlists">
+				<!-- <div class="match" v-for="match in matchlists">
 					<div class="pic">
 						<img v-bind:src="'http://img.wangyuhudong.com'+match.poster" alt="" width="100%">
 						<div class="title"><i class="iconfont"></i><span class="f-col" v-text="match.createDate | formatDate"></span></div>
@@ -26,7 +26,7 @@
 						    <div>by {{match.nickname ? match.nickname : '网娱大师赛事组'}}<span class="f-col f-fr"><strong>{{match.num}}</strong>/{{match.maxNum}}</span></div>
 						</div>
 					</div>			
-				</div>
+				</div> -->
 			</div>
 		</div>
 		<div class="landing-box">
@@ -38,111 +38,22 @@
 						《英雄联盟》秋季超级联赛资格赛报名开启
 					</div>
 				</div>
-				<div class="flip-container">
+				<div class="flip-container" v-for="match in matchlists">
 					<div class="flipper">
 						<div class="front">
 							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
+							<img v-bind:src="'http://img.wangyuhudong.com'+match.poster" width="100%">
 							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
+								<span>{{match.eventName}}</span>
 							</div>
 						</div>
 						<div class="back">
 							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
-						</div>
-					</div>
-				</div>
-				<div class="flip-container">
-					<div class="flipper">
-						<div class="front">
-							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
-							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
-							</div>
-						</div>
-						<div class="back">
-							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
-						</div>
-					</div>
-				</div>
-				<div class="flip-container" style="margin-right:0;">
-					<div class="flipper">
-						<div class="front">
-							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
-							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
-							</div>
-						</div>
-						<div class="back">
-							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
-						</div>
-					</div>
-				</div>
-				<div class="flip-container">
-					<div class="flipper">
-						<div class="front">
-							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
-							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
-							</div>
-						</div>
-						<div class="back">
-							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
-						</div>
-					</div>
-				</div>
-				<div class="flip-container">
-					<div class="flipper">
-						<div class="front">
-							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
-							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
-							</div>
-						</div>
-						<div class="back">
-							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
-						</div>
-					</div>
-				</div>
-				<div class="flip-container" style="margin-right:0;">
-					<div class="flipper">
-						<div class="front">
-							<!-- 前面内容 -->
-							<img src="../../static/images/flip.jpg">
-							<div class="flip-wrap">
-								<span>秋季超级联赛杭州站</span>
-							</div>
-						</div>
-						<div class="back">
-							<!-- 背面内容 -->
-							<h4>网娱大师赛事组</h4>
-							<div class="flip-time"><i class="iconfont">&#xe60f</i>2016/08/07 19:00</div>
-							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>30</strong>/200人</div>
-							<a href="" class="flip-btn"><i class="iconfont">&#xe603</i></a>
+							<h4>{{match.nickname ? match.nickname : '网娱大师赛事组'}}</h4>
+							<div class="flip-time"><i class="iconfont">&#xe60f</i><span v-text="match.createDate | formatDate"></span></div>
+							<div class="flip-num"><i class="iconfont">&#xe619</i><strong>{{match.num}}</strong>/ {{match.maxNum}}人</div>
+							<a href="" class="flip-btn" @click="linkDetail"><i class="iconfont">&#xe603</i></a>
+							<span class="get_eventid">{{match.eventId}}</span><span class="get_roundid">{{match.id}}</span>
 						</div>
 					</div>
 				</div>
@@ -174,7 +85,7 @@ import createPop from '../components/createPop.vue'
 			parm.jsonInfo=JSON.stringify({pageNumber:1});
 	        _this.$http.get('oet/event/getAllEventRoundList',parm).then(function(response) {
 	        	console.log(response);
-	            _this.matchlists=response.data.object.pager.list.slice(0,3);
+	            _this.matchlists=response.data.object.pager.list.slice(0,6);
 	            console.log(_this.matchlists);
 	        },function(response) {
 	            console.log(response);
@@ -194,7 +105,15 @@ import createPop from '../components/createPop.vue'
 			}
 		},
 		methods:{
-			
+			linkDetail:function(e){
+				e.preventDefault();
+				var _target=$(e.currentTarget);
+  				var _eventid=_target.parent().find(".get_eventid").text();
+  				var _roundid=_target.parent().find(".get_roundid").text();
+  				window.sessionStorage.setItem("eventId",_eventid);
+  				window.sessionStorage.setItem("eventRoundId",_roundid);
+  				this.$route.router.go({path: '/matchDetails'})
+			}
 		},
 		events:{
 			
